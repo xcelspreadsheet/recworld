ca5969714723aeb5db932da8bc4c735bf226485c
Cody Ross <xcelspreadsheet@Codys-MBP-4.home>
2020-04-04 16:05:56 -0400

    Updates

---
+++ precache-manifest.ad95bb91efae87e1c28b98f443c4893a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fe9a0bed96691d8a775786f1246539e",
+    "url": "/recworld/index.html"
+  },
+  {
+    "revision": "a6427b8f7802f319bc56",
+    "url": "/recworld/static/css/2.822d6ebf.chunk.css"
+  },
+  {
+    "revision": "b7184c696c27f067c9ed",
+    "url": "/recworld/static/css/main.c2de7cd0.chunk.css"
+  },
+  {
+    "revision": "a6427b8f7802f319bc56",
+    "url": "/recworld/static/js/2.d4f1f7d2.chunk.js"
+  },
+  {
+    "revision": "9bda0cbb0c4216756790e9854498adf1",
+    "url": "/recworld/static/js/2.d4f1f7d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7184c696c27f067c9ed",
+    "url": "/recworld/static/js/main.4f5efddb.chunk.js"
+  },
+  {
+    "revision": "3c08b56f4f4f80941b1a",
+    "url": "/recworld/static/js/runtime-main.ec7b1bc7.js"
+  },
+  {
+    "revision": "19786740d27d9414372b411cdd3af915",
+    "url": "/recworld/static/media/venus.19786740.glb"
+  }
+]);
